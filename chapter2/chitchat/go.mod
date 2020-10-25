5a3e8893711a2274d09d5391ae5275ccb27219cd
neuzen-ryuki <account@ryuki-fujita.xyz>
2020-10-25 23:35:53 +0900

    全然何も解決出来んかったけどとりあえずcommitしとく

---
+++ chapter2/chitchat/go.mod
@@ -0,0 +1,8 @@
+module chitchat
+
+go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/lib/pq v1.8.0
+)
---
+++ chapter2/mysql_setup/setup.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+  id int unsigned PRIMARY KEY AUTO_INCREMENT,
+  uuid varchar(64) NOT NULL UNIQUE,
+  name varchar(255),
+  email varchar(255) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE sessions (
+  id int unsigned PRIMARY KEY AUTO_INCREMENT,
+  user_id int unsigned,
+  uuid varchar(64) NOT NULL UNIQUE,
+  email varchar(255),
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_user_sessions
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE threads (
+  id int unsigned PRIMARY KEY AUTO_INCREMENT,
+  user_id int unsigned,
+  uuid varchar(64) NOT NULL UNIQUE,
+  topic varchar(255),
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_user_threads
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
